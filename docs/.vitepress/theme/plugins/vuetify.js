1258c376bb1a3ad2890bea101662dd0cd120f810
NullPointerException <quark.yeung@icloud.com>
2021-08-11 10:00:47 +0800

    参考了vuetify的issue修复了build错误问题
    https://github.com/vuetifyjs/vuetify/issues/13936

--- docs/.vitepress/theme/plugins/vuetify.js
+++ docs/.vitepress/theme/plugins/vuetify.js
@@ -4,8 +4,8 @@ import 'vuetify/lib/styles/main.sass'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/lib/components'
-import * as directives from 'vuetify/lib/directives'
+import * as components from 'vuetify/lib/components/index.mjs'
+import * as directives from 'vuetify/lib/directives/index.mjs'
 
 export default createVuetify({
   components,
