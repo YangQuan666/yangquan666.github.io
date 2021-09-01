9ff114e9ca1355eb22e7c9b8ae05e4175be2d2dc
NullPointerException <quark.yeung@icloud.com>
2021-09-01 12:58:14 +0800

    更改模块类型为ESM
    添加favicon图标，更新了vuetify版本

---
+++ docs/.vitepress/config.cjs
@@ -0,0 +1,12 @@
+export const title = 'YangQuan\'s blog';
+export const description = 'This is my personal blog site';
+export const lang = 'zh-CN';
+export const head = [
+    ['meta',
+        { charset: 'utf-8' },
+        {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1'
+        }],
+    ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }]
+];
