dce425553bcbc78ee3495b6fcf8b3a7989a07792
NullPointerException <quark.yeung@icloud.com>
2021-09-22 15:43:52 +0800

    优化了默认主题逻辑代码

--- docs/.vitepress/theme/plugins/theme.js
+++ docs/.vitepress/theme/plugins/theme.js
@@ -1,29 +1,19 @@
-import {ref} from "vue";
+import {ref} from 'vue';
 
 export const mode = ref()
 
 export function toggle() {
-    mode.value = mode.value === "light" ? "dark" : "light"
+    mode.value = mode.value === 'light' ? 'dark' : 'light'
 }
 
 export function initTheme() {
-    if (isBrowser()) {
-        const darkMode = window.matchMedia("(prefers-color-scheme: dark)");
+    if (typeof window !== 'undefined') {
+        const darkMode = window.matchMedia('(prefers-color-scheme: dark)');
         mode.value = darkMode.matches ? 'dark' : 'light';
-    }
-}
-
-export function themeChangeListener() {
-    if (isBrowser()) {
-        window.matchMedia("(prefers-color-scheme: dark)")
-            .addEventListener("change", (e) => {
-                mode.value = e.matches ? "dark" : "light";
+        window.matchMedia('(prefers-color-scheme: dark)')
+            .addEventListener('change', (e) => {
+                mode.value = e.matches ? 'dark' : 'light';
             });
     }
 }
 
-
-const isBrowser = () => typeof window !== "undefined"
-
-initTheme()
-themeChangeListener()
--- docs/.vitepress/theme/plugins/vuetify.js
+++ docs/.vitepress/theme/plugins/vuetify.js
@@ -1,7 +1,6 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/lib/styles/main.sass'
-// import 'vuetify/styles' // This is plain css
 
 
 // Vuetify
@@ -9,16 +8,15 @@ import {createVuetify} from 'vuetify'
 import * as components from 'vuetify/lib/components/index.mjs'
 import * as directives from 'vuetify/lib/directives/index.mjs'
 
+import {mode, initTheme} from './theme';
+
+initTheme()
 
 export default createVuetify({
     components,
     directives,
-    defaults: {
-        global: {
-            ripple: true,
-        },
-    },
+    defaults: {},
     theme: {
-        defaultTheme: 'dark',
+        defaultTheme: mode.value,
     }
 })
