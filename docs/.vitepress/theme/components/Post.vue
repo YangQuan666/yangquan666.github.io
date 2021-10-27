25300c4d25684e466e63ebc8a2f926a5eaecab92
NullPointerException <quark.yeung@icloud.com>
2021-10-27 20:12:01 +0800

    将Layout文件的drawer移动到Post文件中

--- docs/.vitepress/theme/components/Post.vue
+++ docs/.vitepress/theme/components/Post.vue
@@ -2,13 +2,21 @@
   <Content/>
   <!-- place QPageScroller at end of page -->
   <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
-    <q-btn fab icon="keyboard_arrow_up" color="accent"/>
+    <q-btn fab icon="keyboard_arrow_up" color="accent" @click="toc = !toc"/>
   </q-page-scroller>
+  <q-drawer
+      v-model="toc"
+      side="right"
+      show-if-above
+      bordered>
+    <!-- table of contents -->
+  </q-drawer>
 </template>
 <script setup>
 import {useData} from 'vitepress';
 import {ref} from 'vue';
 
 const {page} = useData();
-const drawer = ref();
+
+const toc = ref(false)
 </script>
