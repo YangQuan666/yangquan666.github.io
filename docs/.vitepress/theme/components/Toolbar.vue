263d14ddb9f5978e3022c764951aaee5a08153bc
NullPointerException <quark.yeung@icloud.com>
2021-10-27 20:10:36 +0800

    Create Toolbar.vue

---
+++ docs/.vitepress/theme/components/Toolbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="gt-sm">
+    <q-btn-dropdown stretch flat label="Theme" :model-value="value">
+      <q-list>
+        <q-item
+            v-for="theme in themes"
+            :key="theme"
+            @click="theme.trigger"
+            clickable
+            v-close-popup>
+          <q-item-section avatar>
+            <q-avatar :icon="theme.icon"/>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ theme.name }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
+  </div>
+  <div class="lt-sm">
+    <q-btn flat round icon="search"/>
+    <q-btn flat round icon="more_vert"/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {ref} from 'vue';
+import {useQuasar} from 'quasar';
+
+const $q = useQuasar()
+
+const themes = [
+  {
+    'name': 'Auto',
+    'icon': 'brightness_medium',
+    'trigger': () => {
+      $q.dark.set('auto')
+    }
+  }, {
+    'name': 'Light',
+    'icon': 'light_mode',
+    'trigger': () => {
+      $q.dark.set(false)
+    }
+  }, {
+    'name': 'Dark',
+    'icon': 'dark_mode',
+    'trigger': () => {
+      $q.dark.set(true)
+    }
+  }
+]
+const language = [
+  {
+    'name': 'zh_CN',
+  }, {
+    'name': 'en_US',
+  }
+]
+const value = ref('')
+</script>
