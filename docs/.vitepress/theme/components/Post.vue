60977462121c24c368ec344128e5e4a10f9fe8cf
筠之 <yunzhi.yq@antgroup.com>
2022-12-07 23:49:31 +0800

    content使用vp-doc样式渲染文档

--- docs/.vitepress/theme/components/Post.vue
+++ docs/.vitepress/theme/components/Post.vue
@@ -1,27 +1,23 @@
 <template>
-  <Layout>
-    <Content/>
-    <!-- place QPageScroller at end of page -->
-    <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
-      <q-btn fab icon="keyboard_arrow_up" color="accent" @click="toc = !toc"/>
-    </q-page-scroller>
-    <q-drawer
-        v-model="toc"
-        side="right"
-        show-if-above
-        bordered>
-      <!-- table of contents -->
-    </q-drawer>
-  </Layout>
+  <Content class="vp-doc"/>
+  <!-- place QPageScroller at end of page -->
+  <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
+    <q-btn fab icon="keyboard_arrow_up" color="accent" @click="toc = !toc"/>
+  </q-page-scroller>
+  <q-drawer
+      v-model="toc"
+      side="right"
+      show-if-above
+      bordered>
+    <!-- table of contents -->
+  </q-drawer>
 </template>
 <script setup>
-import {useData} from 'vitepress';
-import {ref} from 'vue';
-import DefaultTheme from 'vitepress/theme'
+import {useData} from 'vitepress'
+import {ref} from 'vue'
 
 const {page} = useData();
 
 const toc = ref(false)
 
-const {Layout} = DefaultTheme
 </script>
