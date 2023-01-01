77677739f422e7b178e27f61940ff70e3f8d6eab
筠之 <yunzhi.yq@antgroup.com>
2023-01-01 19:57:58 +0800

    暂时去掉了加载动画，为了不影响页面位置

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,8 +1,6 @@
 import Layout from './Layout.vue'
 import Error404 from './components/Error404.vue'
 
-import {Loading} from 'quasar'
-
 import 'github-markdown-css'
 import './style/custom.css'
 
@@ -15,13 +13,5 @@ export default {
         // app is the Vue 3 app instance from `createApp()`. router is VitePress'
         // custom router. `siteData`` is a `ref`` of current site-level metadata.
         initQuasar(app)
-
-        router.onBeforeRouteChange = () => {
-            Loading.show({message: '(ง •̀_•́)ง (*•̀ㅂ•́)و 努力加载中...'})
-        }
-
-        router.onAfterRouteChanged = () => {
-            Loading.hide()
-        }
     }
 }
