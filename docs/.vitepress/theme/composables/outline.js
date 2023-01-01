89991c9d1cd21d9f20fac168415674bb2318976e
筠之 <yunzhi.yq@antgroup.com>
2023-01-01 17:46:25 +0800

    提交一下

--- docs/.vitepress/theme/composables/outline.js
+++ docs/.vitepress/theme/composables/outline.js
@@ -3,6 +3,7 @@ import {onMounted, onUnmounted, onUpdated, ref} from 'vue';
 // import { throttleAndDebounce } from '../support/utils.js';
 // magic number to avoid repeated retrieval
 export const uniqueItemKey = ref(null)
+
 const PAGE_OFFSET = 71;
 
 export function getHeaders(pageOutline) {
@@ -64,108 +65,4 @@ function addToParent(currIndex, headers, levelsRange) {
     return true;
 }
 
-
-export function useActiveAnchor(container, marker) {
-    const {isAsideEnabled} = {isAsideEnabled: true};
-    const onScroll = throttleAndDebounce(setActiveLink, 100);
-    let prevActiveLink = null;
-    onMounted(() => {
-        requestAnimationFrame(setActiveLink);
-        window.addEventListener('scroll', onScroll);
-    });
-    onUpdated(() => {
-        // sidebar update means a route change
-        activateLink(location.hash);
-    });
-    onUnmounted(() => {
-        window.removeEventListener('scroll', onScroll);
-    });
-
-    function setActiveLink() {
-        if (!isAsideEnabled.value) {
-            return;
-        }
-        const links = [].slice.call(container.value.querySelectorAll('.outline-link'));
-        const anchors = [].slice
-            .call(document.querySelectorAll('.content .header-anchor'))
-            .filter((anchor) => {
-                return links.some((link) => {
-                    return link.hash === anchor.hash && anchor.offsetParent !== null;
-                });
-            });
-        const scrollY = window.scrollY;
-        const innerHeight = window.innerHeight;
-        const offsetHeight = document.body.offsetHeight;
-        const isBottom = Math.abs(scrollY + innerHeight - offsetHeight) < 1;
-        // page bottom - highlight last one
-        if (anchors.length && isBottom) {
-            activateLink(anchors[anchors.length - 1].hash);
-            return;
-        }
-        for (let i = 0; i < anchors.length; i++) {
-            const anchor = anchors[i];
-            const nextAnchor = anchors[i + 1];
-            const [isActive, hash] = isAnchorActive(i, anchor, nextAnchor);
-            if (isActive) {
-                activateLink(hash);
-                return;
-            }
-        }
-    }
-
-    function activateLink(hash) {
-        if (prevActiveLink) {
-            prevActiveLink.classList.remove('active');
-        }
-        if (hash !== null) {
-            prevActiveLink = container.value.querySelector(`a[href="${decodeURIComponent(hash)}"]`);
-        }
-        const activeLink = prevActiveLink;
-        if (activeLink) {
-            activeLink.classList.add('active');
-            marker.value.style.top = activeLink.offsetTop + 33 + 'px';
-            marker.value.style.opacity = '1';
-        } else {
-            marker.value.style.top = '33px';
-            marker.value.style.opacity = '0';
-        }
-    }
-}
-
-function getAnchorTop(anchor) {
-    return anchor.parentElement.offsetTop - PAGE_OFFSET;
-}
-
-function isAnchorActive(index, anchor, nextAnchor) {
-    const scrollTop = window.scrollY;
-    if (index === 0 && scrollTop === 0) {
-        return [true, null];
-    }
-    if (scrollTop < getAnchorTop(anchor)) {
-        return [false, null];
-    }
-    if (!nextAnchor || scrollTop < getAnchorTop(nextAnchor)) {
-        return [true, anchor.hash];
-    }
-    return [false, null];
-}
-
-export function throttleAndDebounce(fn, delay) {
-    let timeoutId;
-    let called = false;
-    return () => {
-        if (timeoutId) {
-            clearTimeout(timeoutId);
-        }
-        if (!called) {
-            fn();
-            called = true;
-            setTimeout(() => {
-                called = false;
-            }, delay);
-        } else {
-            timeoutId = setTimeout(fn, delay);
-        }
-    };
-}
 
