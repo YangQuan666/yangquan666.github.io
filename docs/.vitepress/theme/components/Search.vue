0cf07f7c9290d7164936813fa3c5da6578a05bfe
Yang Quan <quark.yeung@icloud.com>
2023-04-24 19:59:02 +0800

    使用Qselect实现了搜索下拉框

--- docs/.vitepress/theme/components/Search.vue
+++ docs/.vitepress/theme/components/Search.vue
@@ -8,29 +8,62 @@
             <ais-autocomplete>
                 <template v-slot="{ currentRefinement, indices, refine }">
 
-                    <q-input
-                            type="search"
-                            dark
-                            standout bottom-slots
-                            placeholder="Search"
-                            dense hide-bottom-space
+                    <!--                    <q-input-->
+                    <!--                            @blur="searchClose = true"-->
+                    <!--                            @focus="searchClose = false"-->
+                    <!--                            @update:model-value="(value) => {refine(value)}"-->
+                    <!--                            :model-value="currentRefinement"-->
+                    <!--                            type="search"-->
+                    <!--                            clearable-->
+                    <!--                            dark-->
+                    <!--                            standout-->
+                    <!--                            bottom-slots-->
+                    <!--                            dense hide-bottom-space-->
+                    <!--                    >-->
+                    <!--                        <template #prepend>-->
+                    <!--                            <q-icon name="search" class="cursor-pointer"></q-icon>-->
+                    <!--                        </template>-->
+                    <!--                        <template #hint>-->
+                    <!--                            <div style="color: #00b0ff">aaaaaaaa</div>-->
+                    <!--                        </template>-->
+                    <!--                    </q-input>-->
+                    <!--                    <ul v-if="currentRefinement" v-for="index in indices" :key="index.indexId">-->
+                    <!--                        <h2> {{ index.hits.length }}</h2>-->
+                    <!--                        <li>-->
+                    <!--                            <h3>{{ index.indexName }}</h3>-->
+                    <!--                            {{ index }}-->
+                    <!--                        </li>-->
+                    <!--                    </ul>-->
+
+
+                    <q-select
+                            v-model="model"
+                            @input-value="(value) => refine(value)"
+                            :options="getOptions(currentRefinement, indices)"
                             @blur="searchClose = true"
                             @focus="searchClose = false"
-                            @update:model-value="(value) => {refine(value)}"
-                            :model-value="currentRefinement"
+                            label="Search"
+                            behavior="menu"
+                            standout
+                            use-input
+                            hide-dropdown-icon
                     >
-                        <template #prepend>
-                            <q-icon name="search" class="cursor-pointer"></q-icon>
+                        <template v-slot:append>
+                            <q-icon name="search"/>
                         </template>
-                    </q-input>
-
-                    <ul v-if="currentRefinement" v-for="index in indices" :key="index.indexId">
-                        <h2> {{ index.hits.length }}</h2>
-                        <li>
-                            <h3>{{ index.indexName }}</h3>
-                            {{ index }}
-                        </li>
-                    </ul>
+                        <template v-slot:option="scope">
+                            <q-item v-bind="scope.itemProps" @click="router.go(scope.opt)">
+                                {{ scope.opt }}
+                            </q-item>
+                        </template>
+                        <!--                        <template v-slot:no-option>-->
+                        <!--                            <q-item>-->
+                        <!--                                <q-item-section class="text-grey">-->
+                        <!--                                    No results-->
+                        <!--                                </q-item-section>-->
+                        <!--                            </q-item>-->
+                        <!--                        </template>-->
+                    </q-select>
                 </template>
             </ais-autocomplete>
         </ais-instant-search>
@@ -38,10 +71,22 @@
 </template>
 
 <script setup>
-import {ref} from 'vue';
+import {ref, computed} from 'vue';
+import {useRouter} from 'vitepress'
 import {indexName, searchClient} from '../composables/algolia'
 import {AisAutocomplete, AisInstantSearch} from 'vue-instantsearch/vue3/es/index.js'
 
+const router = useRouter()
 const searchClose = ref(true)
+
+const model = ref('')
+
+const getOptions = computed(() => (currentRefinement, indices) => {
+
+    if (!currentRefinement) {
+        return []
+    }
+    return indices[0].hits || [];
+})
 
 </script>
