1c2effad9acc65f1d3ddb65a82109b2739f2ea29
Quan <quark.yeung@icloud.com>
2023-12-05 20:56:07 +0800

    $q.screen有bug，暂时写两份代码吧

--- docs/.vitepress/theme/components/Timeline.vue
+++ docs/.vitepress/theme/components/Timeline.vue
@@ -6,7 +6,25 @@
   </q-img>
   <div class="row animate__animated animate__fadeIn">
     <div class="col-10 offset-1 col-sm-8 offset-sm-2">
-      <q-timeline :layout="layout" color="secondary">
+      <q-timeline layout="comfortable" class="gt-sm" color="secondary">
+        <q-timeline-entry heading>时间轴（{{summaries.length}}）</q-timeline-entry>
+        <q-timeline-entry
+            v-for="summary in summaries"
+            :title="summary.title"
+            :subtitle="summary.time"
+            side="left"
+        >
+          <div class="text-caption">{{ summary.excerpt }}</div>
+          <q-btn
+              flat
+              color="info"
+              @click="router.go(summary.link)"
+          >
+            阅读全文
+          </q-btn>
+        </q-timeline-entry>
+      </q-timeline>
+      <q-timeline layout="dense" class="lt-sm" color="secondary">
         <q-timeline-entry heading>时间轴（{{summaries.length}}）</q-timeline-entry>
         <q-timeline-entry
             v-for="summary in summaries"
@@ -29,13 +47,10 @@
 </template>
 
 <script setup>
-import {computed} from 'vue'
 import {useRouter} from 'vitepress'
 import {useQuasar} from 'quasar'
 import summaries from '../../../post/summary.json'
 
 const router = useRouter()
 const $q = useQuasar()
-
-const layout = computed(() => $q.screen.lt.sm ? 'dense' : 'comfortable')
 </script>
