7eaed5f95a2168c1a205d828489faaa6ba6e48e0
Master Yang <masteryang@didiglobal.com>
2024-04-07 17:05:12 +0800

    add algolia

--- .vitepress/theme/component/Search.vue
+++ .vitepress/theme/component/Search.vue
@@ -1,81 +1,89 @@
 <template>
 
-<!--  <div-->
-<!--      class="search-field"-->
-<!--      v-bind:class='{ "search-closed": searchClose}'-->
-<!--  >-->
-<!--    <ais-instant-search :index-name="indexName" v-bind:search-client="searchClient">-->
-<!--      <ais-autocomplete>-->
-<!--        <template v-slot="{ currentRefinement, indices, refine }">-->
-<!--          <q-select-->
-<!--              v-model="model"-->
-<!--              @input-value="(value) => refine(value)"-->
-<!--              :options="getOptions(currentRefinement, indices)"-->
-<!--              :option-label="opt => opt.title"-->
-<!--              @blur="handleBlur"-->
-<!--              @focus="handleFocus"-->
-<!--              label="Search"-->
-<!--              behavior="menu"-->
-<!--              hide-selected-->
-<!--              dark-->
-<!--              standout-->
-<!--              use-input-->
-<!--              hide-dropdown-icon-->
-<!--          >-->
-<!--            <template v-slot:append>-->
-<!--              <q-icon name="search"/>-->
-<!--            </template>-->
-<!--            <template v-slot:option="scope">-->
-<!--              <q-separator v-if="scope.index > 0" spaced inset/>-->
+  <div
+      class="search-field"
+      v-bind:class='{ "search-closed": searchClose}'
+  >
+    <ais-instant-search index-name="yangquan666.github.io" v-bind:search-client="searchClient">
+      <ais-autocomplete>
+        <template v-slot="{ currentRefinement, indices, refine }">
+          <q-select
+              v-model="model"
+              @input-value="(value) => refine(value)"
+              :options="getOptions(currentRefinement, indices)"
+              :option-label="opt => opt.title"
+              @blur="handleBlur"
+              @focus="handleFocus"
+              label="Search"
+              behavior="menu"
+              hide-selected
+              dark
+              standout
+              use-input
+              hide-dropdown-icon
+          >
+            <template v-slot:append>
+              <q-icon name="search"/>
+            </template>
+            <template v-slot:option="scope">
+              <q-separator v-if="scope.index > 0" spaced inset/>
 
-<!--              <q-item v-bind="scope.itemProps" @click="router.go(scope.opt.link)">-->
-<!--                <q-item-section>-->
-<!--                  <q-item-label>{{ scope.opt.title }}</q-item-label>-->
-<!--                  <q-item-label caption lines="2">{{ scope.opt.excerpt }}</q-item-label>-->
-<!--                </q-item-section>-->
+              <q-item v-bind="scope.itemProps" @click="router.go(scope.opt.link)">
+                <q-item-section>
+                  <q-item-label>{{ scope.opt.title }}</q-item-label>
+                  <q-item-label caption lines="2">{{ scope.opt.excerpt }}</q-item-label>
+                </q-item-section>
 
-<!--                <q-item-section side>-->
-<!--                  <q-item-label caption>-->
-<!--                    {{ date.formatDate(scope.opt.lastUpdated, 'YYYY-MM-DD') }}-->
-<!--                  </q-item-label>-->
-<!--                </q-item-section>-->
-<!--              </q-item>-->
-<!--            </template>-->
-<!--          </q-select>-->
-<!--        </template>-->
-<!--      </ais-autocomplete>-->
-<!--    </ais-instant-search>-->
-<!--  </div>-->
+                <q-item-section side>
+                  <q-item-label caption>
+                    {{ date.formatDate(scope.opt.lastUpdated, 'YYYY-MM-DD') }}
+                  </q-item-label>
+                </q-item-section>
+              </q-item>
+            </template>
+          </q-select>
+        </template>
+      </ais-autocomplete>
+    </ais-instant-search>
+  </div>
 </template>
 
-<!--<script setup>-->
-<!--import {ref, computed} from 'vue'-->
-<!--import {useRouter} from 'vitepress'-->
-<!--import {indexName, searchClient} from '../composable/algolia'-->
-<!--import {AisAutocomplete, AisInstantSearch} from 'vue-instantsearch/vue3/es/index.js'-->
-<!--import {date} from 'quasar'-->
-<!--import {searchClose} from '../composable/store'-->
+<script setup>
+import {ref, computed} from 'vue'
+import {useRouter} from 'vitepress'
 
-<!--const router = useRouter()-->
+import {AisAutocomplete, AisInstantSearch} from 'vue-instantsearch/vue3/es'
+import {date} from 'quasar'
+import {searchClose} from '../composable/store'
+import algoliasearch from "algoliasearch";
 
-<!--const model = ref('')-->
+const router = useRouter()
 
-<!--const getOptions = computed(() => (currentRefinement, indices) => {-->
+const model = ref('')
 
-<!--  if (!currentRefinement) {-->
-<!--    return []-->
-<!--  }-->
-<!--  const opt = indices[0].hits || []-->
-<!--  return opt.sort((a, b) => b.lastUpdated - a.lastUpdated)-->
+const searchClient = algoliasearch(
+    '62QCDY1RP0',
+    '5dd74cb6aebd52dc20601d741451039d'
+)
 
-<!--})-->
+const getOptions = computed(() => (currentRefinement, indices) => {
 
-<!--function handleFocus(){-->
-<!--  searchClose.value = false-->
-<!--}-->
+  if (!currentRefinement) {
+    return []
+  }
+  const opt = indices[0].hits || []
+  return opt.sort((a, b) => b.lastUpdated - a.lastUpdated)
 
-<!--function handleBlur() {-->
-<!--  model.value = ''-->
-<!--  searchClose.value = true-->
-<!--}-->
-<!--</script>-->
+})
+
+function handleFocus() {
+  searchClose.value = false
+}
+
+function handleBlur() {
+  model.value = ''
+  searchClose.value = true
+}
+</script>
+
+
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,12 +4,14 @@ import Layout from './Layout.vue'
 import type {Theme} from 'vitepress'
 import './style.css'
 import {initQuasar} from './composable/quasar'
+import InstantSearch from 'vue-instantsearch/vue3/es'
 
 export default {
     Layout,
     enhanceApp({app, router, siteData}) {
         // ...
         initQuasar(app)
+        app.use(InstantSearch)
     }
 } satisfies Theme
 
