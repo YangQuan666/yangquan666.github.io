06b62c1104debc3cc9b2118689f477e45d503eeb
mike <quark.yeung@icloud.com>
2024-10-30 22:57:42 +0800

    add outline

--- .vitepress/theme/component/Post.vue
+++ .vitepress/theme/component/Post.vue
@@ -1,7 +1,25 @@
 <template>
-  <v-navigation-drawer location="right" v-model="drawer">
-    <v-list>
-      <v-list-item title="Drawer right" value="item01"></v-list-item>
+  <v-navigation-drawer location="right" expand-on-hover v-model="drawer">
+    <v-list density="compact">
+      <v-list-subheader>目录</v-list-subheader>
+      <v-list-item
+          v-for="{ level, title, link } in outline"
+          :value="link" :href="link" :key="title"
+      >
+        <v-list-item-title :class="[
+            'ps-3 text-medium-emphasis',
+            {
+              'text-primary router-link-active': activeItem === link.slice(1),
+              'ps-6': level === 3,
+              'ps-9': level === 4,
+              'ps-12': level === 5,
+              'ps-15': level === 6,
+            }
+          ]">
+          {{ title }}
+        </v-list-item-title>
+      </v-list-item>
+
     </v-list>
   </v-navigation-drawer>
   <v-parallax
@@ -29,4 +47,28 @@ import {useDate} from 'vuetify'
 const date = useDate()
 const {page, frontmatter} = useData()
 const drawer = ref()
+
+const flattenJson = (data) => {
+  const result = []
+  const recursiveFlatten = (items) => {
+    for (const item of items) {
+      // 将当前项目添加到结果中
+      result.push({
+        level: item.level,
+        title: item.title,
+        slug: item.slug,
+        link: item.link
+      })
+      // 如果有子项目，递归调用
+      if (item.children && item.children.length > 0) {
+        recursiveFlatten(item.children)
+      }
+    }
+  }
+  recursiveFlatten(data)
+  return result
+}
+
+const activeItem = ref('')
+const outline = flattenJson(page.value.headers)
 </script>
