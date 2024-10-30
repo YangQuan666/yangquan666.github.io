c280a1a61059357f2665493b7a42199dbbb10779
mike <quark.yeung@icloud.com>
2024-10-30 22:30:09 +0800

    add 404vue

--- .vitepress/theme/component/Error404.vue
+++ .vitepress/theme/component/Error404.vue
@@ -5,20 +5,18 @@
 <template>
   <v-empty-state
       image="/astro-dog.svg"
-      size="200"
-      text-width="250"
   >
     <template v-slot:media>
       <v-img class="mb-8"></v-img>
     </template>
 
     <template v-slot:title>
-      <div class="text-h6 text-high-emphasis">Empty in drafts</div>
+      <div class="text-h6 text-high-emphasis">Page Not Found</div>
     </template>
 
     <template v-slot:text>
       <div class="text-body-1">
-        Save a draft message and it will show up here
+        The page you were looking for does not exist
       </div>
     </template>
   </v-empty-state>
